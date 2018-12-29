b90f05289f6324753412a172ca9dab1557104f92
novandev <donovan.adams@students.makeschool.com>
2018-12-29 13:02:20 -0800

    added more ot the page in terms of styling the navbar now onto the footer

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Navbar,NavItem,Nav,NavDropdown} from 'react-bootstrap';
-import {Link}  from 'react-router-dom';
+import {Link,NavLink}  from 'react-router-dom';
 import './css/header.css'
 class HeaderLayout extends React.Component{
     constructor(props){
@@ -15,20 +15,20 @@ class HeaderLayout extends React.Component{
                     <Navbar id={'navbar'} inverse collapseOnSelect>
                         <Navbar.Header>
                             <Navbar.Brand>
-                                <Link to="/" className="header-brand-link"> <p id='nav-brand-p'>Spacepost</p></Link>
+                                <NavLink to="/" className="header-brand-link"> <p id='nav-brand-p'>Spacepost</p></NavLink>
                             </Navbar.Brand>
                             <Navbar.Toggle />
                         </Navbar.Header>
                         <Navbar.Collapse>
                             <Nav pullRight>
                                 <NavItem eventKey={1} className={'header-nav-item'}>
-                                    <Link to="/spacepostlabs"  className={'header-nav-link'}>Spacepost Labs</Link>
+                                    <NavLink activeStyle={{color: '#D0D0D0',textDecoration: 'none'}}  to="/spacepostlabs"  className={'header-nav-link'}>Spacepost Labs</NavLink>
                                 </NavItem>
                                 <NavItem eventKey={2} href="/about" className={'header-nav-item'}>
-                                    <Link to="/about" className={'header-nav-link'}> About</Link>
+                                    <NavLink activeStyle={{color: '#D0D0D0',textDecoration: 'none'}}  to="/about" className={'header-nav-link'}> About</NavLink>
                                 </NavItem>
                                 <NavItem eventKey={1} href="/contact" className={'header-nav-item'}>
-                                    <Link to="/contact" className={'header-nav-link'}>Contact</Link>
+                                    <NavLink activeStyle={{color: '#D0D0D0',textDecoration: 'none'}}  to="/contact" className={'header-nav-link'}>Contact</NavLink>
 
                                 </NavItem>
                             </Nav>
