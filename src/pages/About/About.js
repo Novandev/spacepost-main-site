be7e7e7adbb23824535f9a0e7369c41802488956
novandev <donovan.adams@students.makeschool.com>
2019-01-02 13:04:45 -0800

    added more to the design

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import {Image} from 'react-bootstrap';
 
-
-
+import './About.css'
+import Stanley from './images/stanley.png';
+import Novan from './images/Novan.jpg'
 class About extends React.Component{
 
 
@@ -10,12 +12,14 @@ class About extends React.Component{
     render(){
 
         return(
-            <section>
+            <section id='about'>
                 <article id='our-vision'>
                     <h2>Spacepost......insert tag line here</h2>
                     <p>
                         Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
                         ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+                        tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                        fringilla. Donec lacinia congue felis in faucibus.Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
                         tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
                         fringilla. Donec lacinia congue felis in faucibus.
                     </p>
@@ -23,6 +27,62 @@ class About extends React.Component{
                 </article>
                 <article id='team'>
                     <h2>Our Team</h2>
+                    <div id='team-section'>
+                        <div>
+                            <Image className='team-img' src={Stanley} responsive />
+                            <h3>Stanley</h3>
+                            <h4>Founder</h4>
+                            <p> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                                ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+                                tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                                fringilla</p>
+                        </div>
+                        <div>
+                            <Image className='team-img' src={Novan} responsive />
+                            <h3>Novan</h3>
+                            <h4>Developer</h4>
+                            <p> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                                ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+                                tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                                fringilla</p>
+                        </div>
+                        <div>
+                            <Image className='team-img' src={Stanley} responsive />
+                            <h3>Stanley</h3>
+                            <h4>Founder</h4>
+                            <p> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                                ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+                                tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                                fringilla</p>
+                        </div>
+                        <div>
+                            <Image className='team-img' src={Novan} responsive />
+                            <h3>Novan</h3>
+                            <h4>Developer</h4>
+                            <p> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                                ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+                                tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                                fringilla</p>
+                        </div>
+                        <div>
+                            <Image className='team-img' src={Stanley} responsive />
+                            <h3>Stanley</h3>
+                            <h4>Founder</h4>
+                            <p> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                                ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+                                tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                                fringilla</p>
+                        </div>
+                        <div>
+                            <Image className='team-img' src={Novan} responsive />
+                            <h3>Novan</h3>
+                            <h4>Developer</h4>
+                            <p> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                                ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+                                tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                                fringilla</p>
+                        </div>
+                    </div>
 
                 </article>
                 <article id='acknowledgements'>
